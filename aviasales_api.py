3056dc364f1290d8d1e94d4c4bfe824ae586b45b
Shmaglit <tatjyana_tikhonova@center.rt.ru>
2019-04-27 13:53:22 +0300

    done2

--- aviasales_api.py
+++ aviasales_api.py
@@ -1,8 +1,8 @@
+#1. Написать функцию получения IATA-кода города из его названия, используя API Aviasales.
+
 import requests
 import json
 
-
-#link2 = 'https://www.travelpayouts.com/widgets_suggest_params?q=Из%20Москвы%20в%20Лондон'
 city = input('Введите название города на русском языке:')
 link = f'http://autocomplete.travelpayouts.com/places2?term={city}&locale=ru&types[]=city'
 data = json.loads(requests.get(link).text, encoding='utf-8 ')
